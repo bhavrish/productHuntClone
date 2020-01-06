ecfbe6eb10a6d327c8c3b8fffb5c6c017ca4beae
Bhavesh Shah <bhavrish@gmail.com>
2020-01-06 16:41:21 -0500

    completed site with homepage + authentication + upvoting system fully working

--- productHuntClone/urls.py
+++ productHuntClone/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from products import views
+from django.conf import settings
+from django.conf.urls.static import static # static is for image, video, etc. files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # connects images in model to media path
--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Products(models.Model):
+	title = models.CharField(max_length=255)
+	url = models.TextField()
+	pub_date = models.DateTimeField()
+	image = models.ImageField(upload_to='images/')
+	icon = models.ImageField(upload_to='images/')
+	body = models.TextField()
+	votes_total = models.IntegerField(default=1)
+	hunter = models.ForeignKey(User, on_delete=models.CASCADE) # stores userID by using foreign key to user model. models.CASCADE deletes post if user is deleted
+
+
+	def __str__(self): # in the admin panel, instead of saying Products(0), Products(1), it says the title of the blogs instead
+		return self.title
+
+	def summary(self):
+		return self.body[:100]+'...'
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
